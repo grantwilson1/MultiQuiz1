ab226b1afd8a2c964b65c6a073172cf96becb91c
grantwilson1 <grantvwilson@icloud.com>
2024-09-03 20:36:14 -0400

    copy my work over

--- app/src/main/java/edu/vt/mobiledev/multiquiz/MainActivity.kt
+++ app/src/main/java/edu/vt/mobiledev/multiquiz/MainActivity.kt
@@ -2,10 +2,84 @@ package edu.vt.mobiledev.multiquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//test
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import com.google.android.material.snackbar.Snackbar
+import edu.vt.mobiledev.multiquiz.databinding.ActivityMainBinding
+
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var option1: Button
+    private lateinit var option2: Button
+    private lateinit var option3: Button
+    private lateinit var option4: Button
+    private lateinit var reset_button: Button
+    private lateinit var help_button: Button
+
+    private val answers = listOf(
+        Answer(R.string.option1, false),
+        Answer(R.string.option2, true),
+        Answer(R.string.option3, false),
+        Answer(R.string.option4, false),)
+
+    private val buttons = listOf(option1, option2, option3, option4)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        option1 = findViewById(R.id.option1)
+        option2 = findViewById(R.id.option2)
+        option3 = findViewById(R.id.option3)
+        option4 = findViewById(R.id.option4)
+        reset_button = findViewById(R.id.reset_button)
+        help_button = findViewById(R.id.help_button)
+
+        binding.option1.setOnClickListener {
+            option1.updateColor()
+        }
+
+        binding.option2.setOnClickListener {
+            option2.updateColor()
+        }
+
+        binding.option3.setOnClickListener {
+            option3.updateColor()
+        }
+
+        binding.option4.setOnClickListener {
+            option4.updateColor()
+        }
+
+        reset_button.setOnClickListener {
+            for (button in buttons){
+                button.isSelected = false
+                button.isEnabled = true
+                button.updateColor()
+            }
+
+        }
+
+        help_button.setOnClickListener {
+            var counter = 0
+            for(answer in answers){
+                if(counter < 2 && !answer.isCorrect){
+                    answer.isEnabled = false
+                    counter++
+                }
+            }
+            for(button in buttons){
+                button.updateColor()
+            }
+        }
+
+
     }
+
+
 }
