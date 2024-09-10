fc7981e629cd4f8c165737e20dbf2141dcc03bac
grantwilson1 <grantvwilson@icloud.com>
2024-09-09 21:27:38 -0400

    changes

--- app/src/main/java/edu/vt/mobiledev/multiquiz/MainActivity.kt
+++ app/src/main/java/edu/vt/mobiledev/multiquiz/MainActivity.kt
@@ -8,7 +8,8 @@ import androidx.activity.enableEdgeToEdge
 import com.google.android.material.snackbar.Snackbar
 import edu.vt.mobiledev.multiquiz.databinding.ActivityMainBinding
 
-
+    //gvwilson
+    //Grant Wilson
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -16,14 +17,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var option2: Button
     private lateinit var option3: Button
     private lateinit var option4: Button
-    private lateinit var reset_button: Button
-    private lateinit var help_button: Button
+    private lateinit var resetButton: Button
+    private lateinit var helpButton: Button
 
     private val answers = listOf(
         Answer(R.string.option1, false),
         Answer(R.string.option2, true),
         Answer(R.string.option3, false),
         Answer(R.string.option4, false),)
+
+        private lateinit var buttons: List<Button>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,43 +39,53 @@ class MainActivity : AppCompatActivity() {
         option2 = findViewById(R.id.option2)
         option3 = findViewById(R.id.option3)
         option4 = findViewById(R.id.option4)
-        reset_button = findViewById(R.id.reset_button)
-        help_button = findViewById(R.id.help_button)
+        resetButton = findViewById(R.id.reset_button)
+        helpButton = findViewById(R.id.help_button)
+
+        buttons = listOf(option1, option2, option3, option4)
+
+        fun updateView(){
+            for(button in buttons){
+                button.updateColor()
+            }
+            helpButton.updateColor()
+        }
 
         binding.option1.setOnClickListener {
             option1.isSelected = !option1.isSelected
-            option1.updateColor()
+            updateView()
         }
 
         binding.option2.setOnClickListener {
             option2.isSelected = !option2.isSelected
-            option2.updateColor()
+            updateView()
         }
 
         binding.option3.setOnClickListener {
             option3.isSelected = !option3.isSelected
-            option3.updateColor()
+            updateView()
         }
 
         binding.option4.setOnClickListener {
             option4.isSelected = !option4.isSelected
-            option4.updateColor()
+            updateView()
         }
 
-        reset_button.setOnClickListener {
-
-
-        }
-
-        help_button.setOnClickListener {
-            var counter = 0
-            for(answer in answers){
-                if(counter < 2 && !answer.isCorrect){
-                    answer.isEnabled = false
-                    counter++
-                }
+        resetButton.setOnClickListener {
+            for(button in buttons){
+                button.isSelected = false
+                button.isEnabled = true
             }
+            helpButton.isSelected = false
+            helpButton.isEnabled = true
+            updateView()
+        }
 
+        helpButton.setOnClickListener {
+            helpButton.isEnabled = false
+            option1.isEnabled = false
+            option3.isEnabled = false
+            updateView()
         }
 
 
